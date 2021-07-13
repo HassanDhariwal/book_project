4658d75b47ae001610c95737d03de230e7716f9b
hassan <hassanriaz2625@gmail.com>
2021-07-13 18:20:27 +0500

    implementation of bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
 
 	def create
 		@book = Book.new(book_params)
+		
 		if @book.save
 			redirect_to books_path
 		else
@@ -19,7 +20,6 @@ class BooksController < ApplicationController
 	end
 
 	def show
-		
 		@book = Book.find(params[:id])
 	end
 
